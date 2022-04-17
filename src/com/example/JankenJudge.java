7b00290cdd2978327badf7096e07f0f29e8a0dc2
本馬遼太郎 <rajiku.blanc@gmail.com>
2022-04-17 19:08:25 +0900

    リファクタリング1-1

--- src/com/example/JankenJudge.java
+++ src/com/example/JankenJudge.java
@@ -1,77 +1,79 @@
 package com.example;
 
 public class JankenJudge {
-    public static void judge(int iUserHand, String sUserHand, int iCompHand, String sCompHand) {
+    public static void judge(int iUserHand, int iCompHand) {
+        // 整数(1~3)を文字(1:グー、2:チョキ、3:パー)に変換
+        String sUserHand = stringHand(iUserHand); // User
+        String sCompHand = stringHand(iCompHand); // Computer
+        // ハンド確認テキスト
         String text = "あなた:" + sUserHand + "相手:" + sCompHand;
         // ユーザーがグーを選択した場合
         if (iUserHand == 1) {
             switch (iCompHand) {
-                case 1 -> {
-                    System.out.println(text);
-                    judgeText("draw");
-                    Janken.janken();
-                }
-                case 2 -> {
-                    System.out.println(text);
-                    judgeText("win");
-                }
-                case 3 -> {
-                    System.out.println(text);
-                    judgeText("lose");
-                }
+                case 1 -> drawText(text);
+
+                case 2 -> winText(text);
+
+                case 3 -> loseText(text);
+
 
             }
         }
         // ユーザーがチョキを選択した場合
         if (iUserHand == 2) {
             switch (iCompHand) {
-                case 1 -> {
-                    System.out.println(text);
-                    judgeText("lose");
-                }
-                case 2 -> {
-                    System.out.println(text);
-                    judgeText("draw");
-                    Janken.janken();
-                }
-                case 3 -> {
-                    System.out.println(text);
-                    judgeText("win");
-                }
+                case 1 -> loseText(text);
+
+                case 2 -> drawText(text);
+
+                case 3 -> winText(text);
+
 
             }
         }
         // ユーザーがパーを出した場合
         if (iUserHand == 3) {
             switch (iCompHand) {
-                case 1 -> {
-                    System.out.println(text);
-                    judgeText("win");
-                }
-                case 2 -> {
-                    System.out.println(text);
-                    judgeText("lose");
-                }
-                case 3 -> {
-                    System.out.println(text);
-                    judgeText("draw");
-                    Janken.janken();
-                }
+                case 1 -> winText(text);
+
+                case 2 -> loseText(text);
+
+                case 3 -> drawText(text);
+
             }
         }
     }
 
-    public static void judgeText(String judge) {
-        // 勝った時に表示される文章
-        String winText = "あなたの勝ちです。おめでとう！";
-        // 負けた時に表示される文章
-        String loseText = "あなたの負けです。残念でした。";
-        // 引き分けの時に表示される文章
-        String drawText = "あいこです。もう１度！";
-        switch (judge) {
-            case "win" -> System.out.println(winText);
-            case "lose" -> System.out.println(loseText);
-            case "draw" -> System.out.println(drawText);
-        }
+    // あいこの場合の処理
+    public static void drawText(String text) {
+        System.out.println(text);
+        System.out.println("あいこです。もう１度！");
+        JankenMain.jankenHand();
     }
+
+    // 勝った場合の処理
+    public static void winText(String text) {
+        System.out.println(text);
+        System.out.println("あなたの勝ちです。おめでとう！");
+    }
+
+    // 負けた場合の処理
+    public static void loseText(String text) {
+        System.out.println(text);
+        System.out.println("あなたの負けです。残念でした。");
+    }
+
+    // 入力された整数を文字列(1=グー、2=チョキ、3=パー)に変換する
+    public static String stringHand(int hand) {
+        String sHand;
+        if (hand == 1) {
+            sHand = "グー";
+        } else if (hand == 2) {
+            sHand = "チョキ";
+        } else {
+            sHand = "パー";
+        }
+        return sHand;
+    }
+
 }
--- src/com/example/Main.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-class Main {
-    public static void main(String[] args) {
-        System.out.println("じゃんけんゲーム開始");
-        Janken.janken();
-        System.out.println("ゲーム終了!");
-    }
-}
